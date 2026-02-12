410acef53b2d84545fd2065f0d65eb45e5ee3ae5
Lorena Almeida <LorenaAlmeida186@gmail.com>
2026-02-12 21:47:01 +0000

    Uma coisa que me incomoda é que você sempre deixa o favicon padrão do fi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,9 @@ export const metadata: Metadata = {
     url: 'https://fran-diarista-goiania.web.app', // Placeholder URL
     siteName: 'Fran Diarista Goiânia',
   },
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%2355C5B9'><path d='M12 22a7 7 0 0 0 7-7c0-2-1-3.9-3-5.5s-3.5-4-4-6.5c-.5 2.5-2 4.9-4 6.5C6 11.1 5 13 5 15a7 7 0 0 0 7 7z'/></svg>",
+  },
 };
 
 const jsonLd = {
